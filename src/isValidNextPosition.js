3832597329da112acd4e87ca57f39f7676f50515
Tiffany <tthuy.hoangt@gmail.com>
2019-11-04 10:40:39 +1100

    update isValidNextPosition function

--- src/isValidNextPosition.js
+++ src/isValidNextPosition.js
@@ -1,25 +1,27 @@
 const isValidNextPosition = (board, position) => { 
-  // CHECK VALID INPUT
-  // not valid input 
   const positionArray = position.split(',').map(Number);
   const positionRow = positionArray[0] - 1;
   const positionColumn = positionArray[1] - 1;
-
+  
+  // not valid input: not 2 integer numbers separated by comma
   if (positionArray.length !== 2 || isNaN(positionRow) || isNaN(positionColumn)){
     return false;
   }
 
-  // not in the board row & column
+  
   const boardHeight = board.length;
   const boardWidth = board[0].length;
+
+  // not valid input: the first number is not positive integer or greater than or equal the number of board row
   if (positionRow < 0 || positionRow >= boardHeight) {
     return false;
   } 
+  // not valid input: the second number is not positive integer or greater than or equal the number of board column
   if (positionColumn < 0 || positionColumn >= boardWidth) {
     return false;
   }
 
-  // the cell is not empty
+  // not valid input: the place entered are already taken
   const selectedCell = board[positionRow][positionColumn];
   if(selectedCell !== '.'){
     return false;
